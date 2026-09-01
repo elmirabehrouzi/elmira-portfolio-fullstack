2ddb0c5ec3682aa8c5fb7fe245efb70139c13674
Elmira Behrouzi <behroozielmira1234@gmail.com>
2026-09-01 14:47:57 +0330

    fix nginx railway backend proxy

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,23 +6,37 @@ from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView,
+    TokenVerifyView
 )
 
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
 
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path(
+        'api/auth/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+
+    path(
+        'api/auth/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+
+    path(
+        'api/auth/token/verify/',
+        TokenVerifyView.as_view(),
+        name='token_verify'
+    ),
 
     path('api/', include('portfolio.urls')),
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+# Media files
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
